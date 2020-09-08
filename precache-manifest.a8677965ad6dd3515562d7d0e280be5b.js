1397e1ec10d8930aa334759056a36f9ef6b489b1
ArtyomLavrishchev <temalggt540@gmail.com>
2020-09-09 00:38:42 +0300

    Updates

---
+++ precache-manifest.a8677965ad6dd3515562d7d0e280be5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f6283e46ab41c287e6292b22ea47606",
+    "url": "/ReactHomeworks/index.html"
+  },
+  {
+    "revision": "9d3b358de71dcb591d56",
+    "url": "/ReactHomeworks/static/css/main.94dc2f22.chunk.css"
+  },
+  {
+    "revision": "332bd11502ae72314e3f",
+    "url": "/ReactHomeworks/static/js/2.ec42496b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ReactHomeworks/static/js/2.ec42496b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d3b358de71dcb591d56",
+    "url": "/ReactHomeworks/static/js/main.bc051690.chunk.js"
+  },
+  {
+    "revision": "83f443f3dbc23cb6d229",
+    "url": "/ReactHomeworks/static/js/runtime-main.96ee1b41.js"
+  }
+]);
