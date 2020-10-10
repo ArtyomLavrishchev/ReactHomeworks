b39eb76efe5731519fe309534a3f35bfe732172a
ArtyomLavrishchev <temalggt540@gmail.com>
2020-10-11 01:31:49 +0300

    task 6

--- src/p2-homeworks/h5/pages/Error404.tsx
+++ src/p2-homeworks/h5/pages/Error404.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-
+import s from "./Error.module.css"
 function Error404() {
     return (
-        <div>
+        <div className={s.err}>
             <div>404</div>
             <div>Page not found!</div>
             <div>—ฅ/ᐠ.̫ .ᐟ\ฅ—</div>
